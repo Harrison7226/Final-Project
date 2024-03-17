4a9edfd5fe4536c2b82e9cdd0558653eba3d33bb
djrosenthal27 <djrosenthal004@gmail.com>
2024-03-16 23:34:59 -0400

    Created Rigged Human Enemies
    Created FBI and SWAT enemy prefabs, which have an idle, aiming, and death animation. Made some changes to the EnemyBehaviour script to accommodate for the new enemy types, but the script still works with Capsule Enemies

--- Assets/Scripts/PrototypeEnemyBehaviour.cs
+++ Assets/Scripts/PrototypeEnemyBehaviour.cs
@@ -8,6 +8,8 @@ public class PrototypeEnemyBehaviour : MonoBehaviour
 
     public bool alive = true; // Still alive?
     public bool canShoot = true; // Can this character attack?
+    public bool riggedEnemy = false; // Rigged enemy or Capsule enemy?
+    public float gunHeight = 0f; // States gun height for ray-tracing purposes. 2f works well for rigged enemies
     
     private Transform targetTransform; // The player's transform. What the enemy will look at
     private ParticleSystem muzzleFlashEffect; // Gun muzzle flash effect
@@ -30,11 +32,31 @@ public class PrototypeEnemyBehaviour : MonoBehaviour
                 && Physics.Raycast(transform.position, targetTransform.position - transform.position, out enemyView, Mathf.Infinity)
                 && enemyView.collider.CompareTag("Player"))
             {
-                transform.LookAt(targetTransform);
+
+                // transform.LookAt(targetTransform); : replaced with code below
+               
+                // This is some code I used in another project that prevents the enemies from
+                // looking upwards when they face the player
+                Vector3 targetPosition = enemyView.collider.gameObject.transform.position;
+                targetPosition.y = transform.position.y;
+                transform.Rotate(0, Vector3.SignedAngle((targetPosition - transform.position).normalized, transform.forward, Vector3.down), 0);
+
+
+                if (GetComponent<Animator>() != null)
+                {
+                    GetComponent<Animator>().SetInteger("AnimState", 1);
+                }
+
+            } else
+            {
+                if (GetComponent<Animator>() != null)
+                {
+                    GetComponent<Animator>().SetInteger("AnimState", 0);
+                }
             }
 
             RaycastHit gunview;
-            if (Physics.Raycast(transform.position, transform.forward, out gunview, Mathf.Infinity))
+            if (Physics.Raycast(transform.position + new Vector3(0, gunHeight, 0), transform.forward, out gunview, Mathf.Infinity))
             {
                 if (gunview.collider.CompareTag("Player"))
                 {
@@ -58,7 +80,7 @@ public class PrototypeEnemyBehaviour : MonoBehaviour
 
             // Actually calculate the gunshot
             RaycastHit gunshot;
-            if (Physics.Raycast(transform.position, transform.forward, out gunshot, Mathf.Infinity))
+            if (Physics.Raycast(transform.position + new Vector3(0, gunHeight, 0), transform.forward, out gunshot, Mathf.Infinity))
             {
                 if (gunshot.collider.CompareTag("Player"))
                 {
@@ -71,9 +93,22 @@ public class PrototypeEnemyBehaviour : MonoBehaviour
     public void Die()
     {
         alive = false;
-        
-        Rigidbody rb = GetComponent<Rigidbody>();
-        rb.constraints = RigidbodyConstraints.None;
-        rb.AddTorque(0, 0, -50, ForceMode.Impulse);
+
+
+
+        if (!riggedEnemy)
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.constraints = RigidbodyConstraints.None;
+            rb.AddTorque(0, 0, -50, ForceMode.Impulse);
+        }
+        else
+        {
+            if (GetComponent<Animator>() != null)
+            {
+                GetComponent<Animator>().SetInteger("AnimState", 2);
+            }
+        }
+
     }
 }
